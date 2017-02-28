6d0cf09f771487e66783a841aa7764e8d2b244a9
Tai @ Flex <dch.tai@gmail.com>
2017-02-28 21:52:31 +0000

    fix deregister function

--- shout_mode.lua
+++ shout_mode.lua
@@ -2,8 +2,13 @@
 -- The default-default mode - just get all connected players
 chat_modes.register("shout", {
 	help = "Send all messages to all players",
-	register = function() end,
-	deregister() = function() end,
+
+	register = function()
+	end,
+
+	deregister = function()
+	end,
+
 	getPlayers = function()
 		return minetest.get_connected_players()
 	end

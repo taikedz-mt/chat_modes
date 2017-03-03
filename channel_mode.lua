b266245c88f902b1c71cc07f3caa2f399780e880
Tai @ Flex <dch.tai@gmail.com>
2017-03-03 11:28:29 +0000

    clean up channel check

--- channel_mode.lua
+++ channel_mode.lua
@@ -7,26 +7,36 @@ local playerschannel = {} -- playername --> channel_name
 -- Define a special moderator channel that cannot be joined by regular players
 local moderatorchannel = minetest.setting_get("chat_modes.channels.moderator") or "moderators"
 
+local function channelcheck(playername, params)
+	if #params < 1 then
+		minetest.chat_send_player("Please specify a channel name")
+		return false
+	end
+	if params[1] == moderatorchannel then
+		return minetest.get_player_privs(playername, {basic_privs=true})
+	else
+		return true
+	end
+end
+
 chat_modes.register_mode("channel", {
 	help = "Send messages to a specific channel only.",
 
 	can_register = function(playername, params)
-		if params[1] == moderatorchannel then
-			return minetest.get_player_privs(playername, {basic_privs=true})
-		else
-			return true
+		if not channelcheck(playername, params) then
+			return false
 		end
+
+		return true
 	end,
 
 	register = function(playername, params)
-		if params[1] == moderatorchannel then
-			if not minetest.get_player_privs(playername, {basic_privs=true}) then
-				return false
-			end
+		if not channelcheck(playername, params) then
+			return false
 		end
 
 		local channelname = params[1]
-		local channelplayers = allchannels[ channelname ]
+		local channelplayers = allchannels[ channelname ] or {}
 
 		channelplayers[playername] = minetest.get_player_by_name(playername)
 		playerschannel[playername] = channelname

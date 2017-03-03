f4945073495843fec886fed7bdf108cdba695a2a
Tai @ Flex <dch.tai@gmail.com>
2017-03-03 12:21:59 +0000

    proximity functionality is now OK

--- proximity_mode.lua
+++ proximity_mode.lua
@@ -2,31 +2,34 @@
 
 local playerproximitypref = {} -- playername --> (int) max message distance
 
-local maxproximity = int(minetest.setting_get("chat_modes.maxproximity") ) or 50
+local maxproximity = tonumber(minetest.setting_get("chat_modes.maxproximity") ) or 50
 
 chat_modes.register_mode("proximity", {
 	help = "Send messages to players within a certain distance",
 
 	register = function(playername, params)
-		local proxpref = int(params[1]) or maxproximity
+		local proxpref = tonumber(params[1]) or maxproximity
 		if proxpref > maxproximity then
 			proxpref = maxproximity
 		end
 
-		playerpxoimitypref[playername] = proxpref
+		playerproximitypref[playername] = proxpref
 	end,
 
 	deregister = function(playername)
-		playerpxoimitypref[playername] = nil
+		playerproximitypref[playername] = nil
 	end,
 
 	getPlayers = function(playername, message)
 		local targetplayers = {}
 		local thisplayer = minetest.get_player_by_name(playername)
 		
+		local i = 1
 		for _,player in pairs(minetest.get_connected_players() ) do
-			if vector.distance(player:getpos(), thisplayer:getpos() ) <= playerpxoimitypref[playername] then
-				targetplayers[#targetplayers] = player
+			if vector.distance(player:getpos(), thisplayer:getpos() ) <= playerproximitypref[playername] then
+				chat_modes.dodebug("Found "..player:get_player_name() )
+				targetplayers[i] = player
+				i = i+1
 			end
 		end
 

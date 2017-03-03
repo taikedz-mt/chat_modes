a5faa4d93fd3579f17ce7b6c9d6e722374b70de9
Tai @ Flex <dch.tai@gmail.com>
2017-03-03 17:28:15 +0000

    expose deafness check

--- deaf_interceptor.lua
+++ deaf_interceptor.lua
@@ -1,11 +1,15 @@
 -- Keep track of players who have deafened themselves
 local deafplayers = {}
 
+function chat_modes.isdeaf(playername)
+	return deafplayers[playername]
+end
+
 minetest.register_chatcommand("deaf", {
 	description = "Toggle deaf status. If you are deaf (Deaf mode 'on'), you do not receive any chat messages.",
 	privs = {shout = true},
 	func = function(playername, args)
-		if deafplayers[playername] then
+		if isdeaf(playername) then
 			deafplayers[playername] = nil
 			minetest.chat_send_player(playername, "Deaf mode OFF")
 		else
@@ -19,7 +23,7 @@ chat_modes.register_interceptor("deaf mode", function(sender, message, targets)
 	local newtargets = {}
 
 	for _,player in pairs(targets) do
-		if not deafplayers[player:get_player_name()] then
+		if not isdeaf(player:get_player_name() ) then
 			newtargets[#newtargets+1] = player
 		end
 	end
